b4154947f9989f2cb2951434a5dd6fff2e189bed
heestogram <anthjoon11@naver.com>
2023-08-11 11:10:24 +0900

    try1

--- [KUBIG]CV_Project/json2txt/json2txt.py
+++ [KUBIG]CV_Project/json2txt/json2txt.py
@@ -1,27 +1,29 @@
 import json
 import os
 
-directory = 'annotations'
+directory = '[KUBIG]CV_Project/json2txt/annotations'
+
+print(os.getcwd())
+
 for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    data = json.load(f)
+    
+    with open(os.path.join(directory, filename), 'r', encoding='UTF8') as f:
+        data = json.load(f)
 
-    for entry in data:
+        annotations = data['ANNOTATION_INFO']
+        txt_path = os.path.join('[KUBIG]CV_Project/json2txt/labels',filename.replace('.json','.txt'))
 
-        annotations = entry['ANNOTATION_INFO']
-
-        txt_path = os.path.join('labels',filename.replace('.json','.txt'))
-
-        with open(txt_path, 'w') as f
+        with open(txt_path, 'w') as f:
             for ann in annotations:
+                print(ann)
                 class_id = 0
-                img_width = entry['IMAGE_INFO']['IMAGE_WIDTH']
-                img_height = entry['IMAGE_INFO']['IMAGE_HEIGHT']
+                img_width = data['IMAGE_INFO']['IMAGE_WIDTH']
+                img_height = data['IMAGE_INFO']['IMAGE_HEIGHT']
 
                 #points가 의미하는 게 뭐지?
-                x_center = (ann['POINSTS'][0][0] + ann['POINSTS'][0][1]) / 2.0 / img_width
-                y_center = (ann['POINSTS'][0][2] + ann['POINSTS'][0][3]) / 2.0 / img_height
-                width = (ann['POINSTS'][0][1] - ann['POINSTS'][0][0]) / img_width
-                height = (ann['POINSTS'][0][3] - ann['POINSTS'][0][2]) / img_height
+                x_center = (ann['POINTS'][0][0] + ann['POINTS'][0][1]) / 2.0 / img_width
+                y_center = (ann['POINTS'][0][2] + ann['POINTS'][0][3]) / 2.0 / img_height
+                width = (ann['POINTS'][0][1] - ann['POINTS'][0][0]) / img_width
+                height = (ann['POINTS'][0][3] - ann['POINTS'][0][2]) / img_height
 
                 f.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
