f911f1dfdd8b66975c0650f78c7738259da30a88
heestogram <anthjoon11@naver.com>
2023-08-11 11:49:44 +0900

    json2txt try

--- [KUBIG]CV_Project/json2txt/json2txt.py
+++ [KUBIG]CV_Project/json2txt/json2txt.py
@@ -15,15 +15,14 @@ for filename in os.listdir(directory):
 
         with open(txt_path, 'w') as f:
             for ann in annotations:
-                print(ann)
                 class_id = 0
                 img_width = data['IMAGE_INFO']['IMAGE_WIDTH']
                 img_height = data['IMAGE_INFO']['IMAGE_HEIGHT']
 
                 #points가 의미하는 게 뭐지?
-                x_center = (ann['POINTS'][0][0] + ann['POINTS'][0][1]) / 2.0 / img_width
-                y_center = (ann['POINTS'][0][2] + ann['POINTS'][0][3]) / 2.0 / img_height
-                width = (ann['POINTS'][0][1] - ann['POINTS'][0][0]) / img_width
-                height = (ann['POINTS'][0][3] - ann['POINTS'][0][2]) / img_height
+                x_center = abs(ann['POINTS'][0][0] + ann['POINTS'][0][2]) / 2.0 / img_width
+                y_center = abs(ann['POINTS'][0][1] + ann['POINTS'][0][3]) / 2.0 / img_height
+                width = abs(ann['POINTS'][0][2] - ann['POINTS'][0][0]) / img_width
+                height = abs(ann['POINTS'][0][3] - ann['POINTS'][0][1]) / img_height
 
                 f.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
